d71704677567d03661fd10f66d7aec727441386d
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2025-05-05 20:32:27 -0400

    variables set up

--- FileSyncClient.java
+++ FileSyncClient.java
@@ -3,3 +3,14 @@ import java.net.*;
 import java.nio.file.*;
 import java.util.*;
 import static java.nio.file.StandardWatchEventKinds.*;
+
+public class FileSyncClient {
+    private static final String SERVER_HOST = "localhost";
+    private static final int SERVER_PORT = 8000;
+    private static final String DIRECTORY = "client_files";    
+    private Map<String, Long> fileModificationTimes = new HashMap<>(); //existing files
+    private Socket socket;
+    private ObjectInputStream input;
+    private ObjectOutputStream output;
+    
+}
